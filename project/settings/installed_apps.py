a0318e9b0b2cec7d69ad85f9f051d6947849f584
soraiapacheco <soraiapas@gmail.com>
2023-06-13 12:07:05 +0100

    initial commit rest framework changing the views in recipes app

---
+++ project/settings/installed_apps.py
@@ -0,0 +1,14 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # Django rest framework
+    'rest_framework',
+    # Estes são nossos apps
+    'recipes',
+    'authors',
+    'tag',
+]
---
+++ recipes/views/api.py
@@ -0,0 +1,8 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+# @api_view(http_method_names=['get', 'post'])
+@api_view()
+def recipe_api_list(request):
+    return Response({"name": "blablabla"})
