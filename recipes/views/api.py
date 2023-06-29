e9603032268f160fda055ed1bb54a541d18ebb90
soraiapacheco <soraiapas@gmail.com>
2023-06-29 12:40:23 +0100

    creating our permissions and using them

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,13 +1,14 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from tag.models import Tag
 
 from ..models import Recipe
+from ..permissions import IsOwner
 from ..serializers import RecipeSerializer, TagSerializer
 
 
@@ -19,7 +20,7 @@ class RecipeAPIV2ViewSet(ModelViewSet):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIV2Pagination
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
 
     def get_queryset(self):
         qs = super().get_queryset()
@@ -31,6 +32,29 @@ class RecipeAPIV2ViewSet(ModelViewSet):
         # print('Category id:', category_id)
 
         return qs
+
+    def get_object(self):
+        pk = self.kwargs.get('pk', '')
+        obj = get_object_or_404(
+            self.get_queryset(),
+            pk=pk)
+
+        self.check_object_permissions(self.request, obj)
+
+        return obj
+
+    def list(self, request, *args, **kwargs):
+        print('REQUEST: ', request.user, self.request.user)
+        print(request.user.is_authenticated)
+
+        return super().list(request, *args, **kwargs)
+
+    def get_permissions(self):
+        if self.request.method in ['PATCH', 'DELETE']:
+            return [IsOwner(), ]
+        if self.request.method == 'POST':
+            return []
+        return super().get_permissions()
 
 
 @api_view()
